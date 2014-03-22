d1493009c369a61afb8a219341741bef458a908d
siavash9000 <siavash@siavash-ThinkPad-X230.(none)>
2014-03-22 07:25:24 +0100

    added values from website to unit test

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -10,8 +10,8 @@ public class BoardTest {
 		Board one = new Board(new int[][]{{1,2,3},{4,5,6},{7,8,0}});
 		assertEquals(0,one.hamming());
 		assertEquals(3,one.dimension());
-		Board two = new Board(new int[][]{{0,2,3},{4,5,6},{7,8,1}});
-		assertTrue(two.hamming() == 1);
+		Board two = new Board(new int[][]{{8,1,3},{4,0,2},{7,6,5}});
+		assertTrue(two.hamming() == 5);
 		Board three = new Board(new int[][]{{0,1,2},{3,4,5},{6,7,8}});
 		assertTrue(three.hamming() == 8);
 		Board four = new Board(new int[][]{{0,2,3},{4,6,5},{7,8,1}});
@@ -23,8 +23,8 @@ public class BoardTest {
 		Board one = new Board(new int[][]{{1,2,3},{4,5,6},{7,8,0}});
 		assertEquals(0,one.manhattan());
 		assertEquals(3,one.dimension());
-		Board two = new Board(new int[][]{{0,2,3},{4,5,6},{7,8,1}});
-		assertTrue(two.manhattan() == 4);
+		Board two = new Board(new int[][]{{8,1,3},{4,0,2},{7,6,5}});
+		assertTrue(two.manhattan() == 10);
 		Board three = new Board(new int[][]{{0,1,2},{3,4,5},{6,7,8}});
 		assertTrue(three.manhattan() == 12);
 		Board four = new Board(new int[][]{{0,2,3},{4,6,5},{7,8,1}});
